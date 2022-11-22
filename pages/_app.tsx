12bbf31427be8f2fb97168a689f621c862b5cd93
haoyudong <2070343054@email.szu.edu.cn>
2022-11-22 23:24:54 +0800

    style: add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import localFont from '@next/font/local';
+const smileySans = localFont({ src: '../public/SmileySans-Oblique.ttf.woff2' });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={smileySans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
