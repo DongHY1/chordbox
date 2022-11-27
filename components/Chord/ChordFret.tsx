9db572655ebfbb5d32af12cf459d712a35d7e387
haoyudong <2070343054@email.szu.edu.cn>
2022-11-27 15:40:24 +0800

    add: click button

--- components/Chord/ChordFret.tsx
+++ components/Chord/ChordFret.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cross from '../../public/cross.svg';
 import Image from 'next/image';
 export default function ChordFret() {
+  const [isClick, setIsClick] = useState(false);
   return (
-    <button className="border-b border-indigo-600">
-      <div className="flex justify-center">
-        <Image src={cross} className="w-3 h-3 " alt="cross svg" />
+    <button
+      className="border-b border-indigo-600 "
+      onClick={() => setIsClick(!isClick)}
+    >
+      <div className="flex justify-center ">
+        {isClick ? (
+          <div className="w-3 h-3 border rounded-full border-gray-900"></div>
+        ) : (
+          <Image src={cross} className="w-3 h-3 " alt="cross svg" />
+        )}
       </div>
     </button>
   );
