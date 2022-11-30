7c8013b479379b93326d3a107d013dd94f342964
haoyudong <2070343054@email.szu.edu.cn>
2022-11-30 09:43:07 +0800

    chore:add component css in cypress

--- cypress/component/Main.cy.tsx
+++ cypress/component/Main.cy.tsx
@@ -1,5 +1,10 @@
 import Main from '../../components/Main';
 describe('Main.cy.ts', () => {
+  beforeEach(() => {
+    // run these tests as if in a desktop
+    // browser with a 720p monitor
+    cy.viewport('ipad-mini');
+  });
   it('playground', () => {
     cy.mount(<Main />);
   });
